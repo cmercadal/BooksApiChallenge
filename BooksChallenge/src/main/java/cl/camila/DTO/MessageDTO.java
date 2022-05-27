c7020c62505e3ba85a66ee7a296c224736e5e5fa
cmercad <camilamercadal@gmail.com>
2022-05-27 16:21:29 -0400

    Message DTO and Unitary Test that doesnt work

---
+++ BooksChallenge/src/main/java/cl/camila/DTO/MessageDTO.java
@@ -0,0 +1,15 @@
+package cl.camila.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageDTO {
+	
+	private String message;
+	
+
+}
--- BooksChallenge/src/test/java/cl/camila/BooksChallengeApplicationTests.java
+++ BooksChallenge/src/test/java/cl/camila/BooksChallengeApplicationTests.java
@@ -1,13 +1,41 @@
 package cl.camila;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import cl.camila.model.Book;
+import cl.camila.service.BookService;
+import junit.framework.TestCase;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {TestCase.class, WebAppConfiguration.class})
+@WebAppConfiguration
 class BooksChallengeApplicationTests {
 
+	//	private static RepositorioPersona repositorioPersona = mock(RepositorioPersona.class); //se le pasa la clase pero sin la palabra Test
+
+	@Autowired
+	BookService bookServiceMock;
+	
 	@Test
-	void contextLoads() {
+	public void findByIdTest() {
+		Book first = new Book();
+		first.setBookId(1);
+		first.setTitle("Dune");
+		first.setAuthor("Frank Herbert");
+		first.setLocation("1a");
+		
+		when(bookServiceMock.findById(1)).thenReturn(Optional.of(first));
+		
 	}
 
 }
