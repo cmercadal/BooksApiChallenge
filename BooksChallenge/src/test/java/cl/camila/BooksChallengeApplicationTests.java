78fd2d22e3229eccc8921258467bece177f5667f
cmercad <camilamercadal@gmail.com>
2022-06-07 14:22:21 -0400

    Test problema con Optional

--- BooksChallenge/src/test/java/cl/camila/BooksChallengeApplicationTests.java
+++ BooksChallenge/src/test/java/cl/camila/BooksChallengeApplicationTests.java
@@ -1,41 +1,88 @@
 package cl.camila;
 
+import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.Optional;
 
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import cl.camila.model.Book;
-import cl.camila.service.BookService;
-import junit.framework.TestCase;
+import cl.camila.DTO.BookDTO;
+import cl.camila.controller.BookController;
+import cl.camila.service.BookServiceImpl;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {TestCase.class, WebAppConfiguration.class})
-@WebAppConfiguration
+@WebMvcTest(BookController.class)
 class BooksChallengeApplicationTests {
-
-	//	private static RepositorioPersona repositorioPersona = mock(RepositorioPersona.class); //se le pasa la clase pero sin la palabra Test
-
-	@Autowired
-	BookService bookServiceMock;
 	
-	@Test
-	public void findByIdTest() {
-		Book first = new Book();
-		first.setBookId(1);
-		first.setTitle("Dune");
-		first.setAuthor("Frank Herbert");
-		first.setLocation("1a");
+	@Autowired
+	BookController bookController;
+	
+	@Autowired
+	MockMvc mockMvc;
+	
+	@Mock
+	BookServiceImpl bookService;
+	
+	
+	@Test	
+	void getUserByIdTest() {
 		
-		when(bookServiceMock.findById(1)).thenReturn(Optional.of(first));
+		//mock the data return by the user service class
+		BookDTO book = new BookDTO();
+		book.setTitle("Dune");
+		book.setAuthor("Frank Hebert");
+		book.setLocation("1a");
 		
+		when(bookService.findById(anyInt())).thenReturn(Optional.of(book));
+		
+		//create a mock Http request to verify the expected result
+		
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/books/1"))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.Title").value("Dune"))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.Author").value("Frank Hebert"))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.Location").value("1a"))
+		.andExpect(status().isOk());
 	}
+	
+	
+	
+
+//	@Mock
+//	BookService bookServiceMock;
+//		
+//    @InjectMocks
+//    BookController bookController = new BookController();
+//
+//    MockMvc mockMvc;
+//    
+//    @Before
+//    public void getATestBook() {
+//    	BookDTO testBook = new BookDTO();
+//    	
+//    }
+//    
+//    @Before
+//    public void setup(){
+//        mockMvc = MockMvcBuilders.standaloneSetup(bookController).build();
+//    }
+//	
+//    @Test
+//    public void testInsertStudent(){
+//
+//        BookDTO book = new BookDTO();
+//
+//        bookController.save(book);
+//
+//        verify(bookServiceMock, times(1)).save(book);
+//    }
+//	
+
 
 }
